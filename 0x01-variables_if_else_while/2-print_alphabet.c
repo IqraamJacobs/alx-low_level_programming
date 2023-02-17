73adbdc2796b919f66f43f550f7b0b1aeb5cfb2a
IqraamJacobs <iqraamjacobs@gmail.com>
2023-02-17 14:34:35 -0800

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,20 @@
-#!/bin/bash
-julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-print_alphabet.c -o 2-print_alphabet
-julien@ubuntu:~/0x01$ ./2-print_alphabet
-abcdefghijklmnopqrstuvwxyz
-julien@ubuntu:~/0x01$
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        char lc;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+        return (0);
+}
