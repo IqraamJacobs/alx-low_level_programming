4bcf2ce5d18cbbf6103d1817f6db2d2e1cc37ca5
IqraamJacobs <iqraamjacobs@gmail.com>
2023-02-17 15:56:22 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,17 @@
-#!/bin/bash
-julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-print_numbers.c -o 5-print_numbers
-julien@ubuntu:~/0x01$ ./5-print_numbers
-0123456789
-julien@ubuntu:~/0x01$
+#include <stdio.h>
+
+/**
+ * main - print all strings from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (90);
+}	
