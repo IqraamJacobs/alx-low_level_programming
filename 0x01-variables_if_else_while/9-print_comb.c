b4147bdcc20169e1d973db5b2c9b592391cadef4
IqraamJacobs <iqraamjacobs@gmail.com>
2023-02-17 15:49:19 -0800

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,23 @@
-#!/bin/bash
-julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-print_comb.c -o 9-print_comb
-julien@ubuntu:~/0x01$ ./9-print_comb | cat -e
-0, 1, 2, 3, 4, 5, 6, 7, 8, 9$
-julien@ubuntu:~/0x01$
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+        {
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+	}
