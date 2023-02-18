f41268f14d5bbe975a75397361a804736a1d5d90
IqraamJacobs <iqraamjacobs@gmail.com>
2023-02-17 16:26:09 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,27 @@
-#!/bin/bash
-julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-print_base16.c -o 8-print_base16
-julien@ubuntu:~/0x01$ ./8-print_base16
-0123456789abcdef
-julien@ubuntu:~/0x01$
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}	
