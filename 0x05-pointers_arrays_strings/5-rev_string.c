17e5fdb42028d80f766632de1b65f948d8d9f050
Iqraam Jacobs <iqraamjacobs@gmail.com>
2023-02-27 20:33:00 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,18 +5,16 @@
  *
  *@s: this is the pointer that point to a string
  */
-
 void rev_string(char *s)
 {
 	int a, len;
 
+	char *begin, x, *end = s;
 	char *begin, *end = s;
 
 	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
 	{
-		end++;
-	}
-	len = a + 1;
+@@ -20,6 +20,7 @@ void rev_string(char *s)
 	begin = s;
 	for (a = 0; a < len / 2; a++)
 	{
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * print_array - function that prints n elements of an array of integers, followed by a new line.
+ * print_array - function that prints n elements of an array of integers,
+ * followed by a new line.
  * @a: This is the input array
- * @n: This is the length of the array
+ * @n: This is the lenght of the array
  *
  *
  */
@@ -11,13 +12,13 @@
 void print_array(int *a, int n)
 {
 	int index;
-
+	
 	for (index = 0; index < n; index++)
 	{
-		printf("%d", a[index];
+		printf("%d", a[index]);
 		if (index != (n - 1))
 		{
-			printf(",");
+			printf(", ");
 		}
 	}
 	putchar('\n');
